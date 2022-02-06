dd37e427044b96796ac277751d6f0dfe4deeac4b
Joshua Kordani <joshua.kordani@gmail.com>
2022-02-06 14:01:00 -0500

    Added functions and .gitignore

--- josh-stats.lisp
+++ josh-stats.lisp
@@ -35,23 +35,36 @@
 					  (second ft))))
 	       ft)  #'<))
 
-(defun 5-finger-death-punch (d)
+(defun analysis (d)
   (let* ((data
 	  (freak-expand d))
 	 (quants (quantile data))
 	 (iqr (- (third quants) (first quants))))
-    
+
     (reverse
-     (pairlis '(:min :q :max :iqr :under-over)
-	      (list (reduce #'min data)
-		    quants
-		    (reduce #'max data)
-		    iqr
-		    (over-under (third quants)
-				(first quants)))))))
+     (pairlis '(:mean :median :mode :min :q :max :iqr :under-over)
+	      (list
+	       (mean data)
+	       (median data)
+	       (mode data)
+	       (reduce #'min data)
+	       quants
+	       (reduce #'max data)
+	       iqr
+	       (over-under (third quants)
+			   (first quants)))))))
 
 (defun mean (data)
   (float (/ (reduce #'+ data) (length data))))
+
+;gross
+(defun mode (data)
+  (let ((values (remove-duplicates data)))
+    (pairlis '(:value :count)
+	     (first (sort (loop for value in values
+				collect (list value (count value data)))
+			  #'> :key #'second)))))
+
 
 (defun variance (data &optional (p nil))
   (let ((mean (mean data)))
@@ -69,3 +82,13 @@
 
 (defun chebyshev (k)
   (- 1 (/ 1 (expt k 2))))
+
+(defun chebyshev-plot (count &optional (step .1))
+  (let ((range (range 1.01 count step)))
+    (vgplot:plot range
+		 (mapcar #'chebyshev range))))
+
+(defun range (start count &optional (step 1))
+  (loop
+    for i from start to count by step
+    collect i))
